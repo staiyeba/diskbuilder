b0f047e63e38ec2111ffd676f40f84e43b75876b
taiyeba <staiyeba@gmail.com>
2019-04-02 10:26:02 +0200

    travis: fixing python version

--- .ci/install.sh
+++ .ci/install.sh
@@ -5,6 +5,5 @@ set -x
 if [[ "$(uname -s)" == 'Darwin' ]]; then
     brew update || brew update
     brew install cmake || true
-    brew install python3 || true
 fi
-pip3 install conan --upgrade
+pip install conan --upgrade
